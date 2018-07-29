bffc8a3485162c7777a8dabe53142b8dca3fc00d
guofazhan <guofazhan@hotmail.com>
2018-07-29 22:24:35 +0800

    添加基础功能控制层以及展现页面

---
+++ genealogy-admin/src/main/java/com/genealogy/admin/web/controller/report/StatisticalReportController.java
@@ -0,0 +1,26 @@
+package com.genealogy.admin.web.controller.report;
+
+import com.genealogy.admin.web.controller.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 统计报表管理控制层
+ * @author G2Y
+ * @version [版本号, 2018/7/29 0001]
+ * @see [相关类/方法]
+ * @since [产品/模块版本]
+ */
+@Controller
+@RequestMapping("/report/sr")
+public class StatisticalReportController extends BaseController {
+
+    private static final String PREFIX = "report/sr";
+
+    @GetMapping()
+    String index(Model model) {
+        return PREFIX + "/index";
+    }
+}
