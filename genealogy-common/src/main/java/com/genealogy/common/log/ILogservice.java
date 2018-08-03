880ea7df9dea97ac3f2a833490d61ea3c2affdc1
guofazhan <guofazhan@leadeon.cn>
2018-08-03 17:11:08 +0800

    优化目录结构以及数据接口

---
+++ genealogy-common/src/main/java/com/genealogy/common/log/ILogservice.java
@@ -0,0 +1,23 @@
+package com.genealogy.common.log;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+/**
+ * 日志接口
+ *
+ * @author guofazhan
+ * @version [版本号, 2018/8/3 0003 0001]
+ * @see [相关类/方法]
+ * @since [产品/模块版本]
+ */
+public interface ILogservice {
+
+	/**
+	 * 日志统计接口
+	 *
+	 * @param operation 操作描述
+	 * @param point
+	 * @param time 耗时
+	 */
+	void log(String operation, ProceedingJoinPoint point, long time);
+}
